cc3cbfbd4ab22b9696b30e4d0631f7d132838922
sunilpulugula <sunil.pulugula@imaginea.com>
2014-09-13 18:57:02 +0530

    adding gzip sources

---
+++ GZIPFilter.java
@@ -0,0 +1,46 @@
+
+package com.wavemaker.runtime.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.wavemaker.runtime.GZIPResponseWrapper;
+
+/**
+ * @author Sunil Kumar
+ */
+public class GZIPFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res,
+                         FilterChain chain) throws IOException, ServletException {
+        if (req instanceof HttpServletRequest) {
+            HttpServletRequest request = (HttpServletRequest) req;
+
+            HttpServletResponse response = (HttpServletResponse) res;
+            String ae = request.getHeader("accept-encoding");
+            if (ae != null && ae.indexOf("gzip") != -1) {
+                GZIPResponseWrapper wrappedResponse = new GZIPResponseWrapper(response);
+                chain.doFilter(req, wrappedResponse);
+                wrappedResponse.finishResponse();
+                return;
+            }
+            chain.doFilter(req, res);
+        }
+    }
+
+    public void init(FilterConfig filterConfig) {
+        // no code
+    }
+
+    public void destroy() {
+        // no code
+    }
+}
